7c38c422e56ec4f0be1731f271061a6d6d86db71
Shoumeiki <shoumeiki@gmail.com>
2025-09-24 21:20:16 +1000

    Fixed issue with flux bootstrap generated files

--- infrastructure/nixos/modules/services/flux.nix
+++ infrastructure/nixos/modules/services/flux.nix
@@ -37,8 +37,8 @@ in
 
       path = mkOption {
         type = types.str;
-        default = "infrastructure/kubernetes/flux-system";
-        description = "Path within repository for Flux system manifests";
+        default = "infrastructure/kubernetes";
+        description = "Path within repository containing flux-system directory";
       };
     };
 
